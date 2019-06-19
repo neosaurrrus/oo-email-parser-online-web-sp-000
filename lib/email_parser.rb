7cbd2c978c87de75891e6b9f8bee85f4c9ad094b
Lukie Kang <lukiek+f@gmail.com>
2019-06-19 20:38:50 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,8 @@ class EmailParser
   def parse
     commas_out_emails = @email_string.gsub(',', ' ')
     doublespace_out_emails = commas_out_emails.gsub('  ', ' ')
-    emails_array= @doublespace_out_emails.split(" ");
+    puts doublespace_out_emails
+    emails_array= doublespace_out_emails.split(" ");
 
     puts emails_array
     # short_parsed_emails = parsed_emails.filter do |email|
